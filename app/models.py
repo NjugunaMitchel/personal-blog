e23bb7a978e2005a7f5541082df49002f1026a42
mishel254 <sonimichie@gmail.com>
2020-11-03 14:24:20 +0300

    models structure

---
+++ app/models.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from flask_login import LoginManager
+from flask_login import UserMixin, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+from .import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique = True, nullable=False)
+    email = db.Column(db.String(255), unique = True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    photo = db.Column(db.String(255), nullable=False, default = 'image.jpg')
+    bio = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='author', lazy=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f"Users('{self.username}','{self.email}','{self.photo}')"
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date}')"
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('Users',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'Users {self.name}'
